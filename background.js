7e64cbc21d32b95219cbedab1045483ae8769152
john <johnathan.stephens@protonmail.com>
2025-07-28 23:06:39 +0200

    fix focus bug

--- background.js
+++ background.js
@@ -28,6 +28,7 @@ browser.commands.onCommand.addListener(async (command) => {
       break;
   }
   //store_update_schtick
+  console.log();
 });
 
 
@@ -54,8 +55,11 @@ async function workspace_schtick(next_workspace) {
   const next_workspace_length = workspaces['tab_counts'][next_workspace];
 
   if (next_workspace_length == 0) {
-    await browser.tabs.create({index: -1}); // doesn't state default; or that -1=last
+    const new_tab = await browser.tabs.create({}); // can't set -1
+    await browser.tabs.move(new_tab.id, { index: -1 });
+    workspaces.tab_counts[next_workspace] = 1;
   } else {
+
     const next_workspace_start = get_start_index_of_workspace(next_workspace);
     const next_workspace_end = next_workspace_start + next_workspace_length;
     const next_workspace_tabs = await get_tabs_by_index(next_workspace_start, next_workspace_end);
@@ -63,10 +67,12 @@ async function workspace_schtick(next_workspace) {
 
     await browser.tabs.move(next_workspace_tab_ids, { index: -1 });
     await browser.tabs.show(next_workspace_tab_ids);
+    await browser.tabs.update(next_workspace_tab_ids[0], { active: true });
   }
 
   // finishing
   await browser.tabs.hide(visible_tab_ids);
+  console.log("end");
 }
 
 
