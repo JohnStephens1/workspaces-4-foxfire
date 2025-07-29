9957ebbef1052f21b86d3127733fed5b1c78d36c
john <johnathan.stephens@protonmail.com>
2025-07-29 19:48:46 +0200

    environment updates

--- background.js
+++ background.js
@@ -7,6 +7,7 @@
 
 
 let workspaces = get_default_workspaces();
+initial_storage_check().catch(console.error);
 
 
 function get_default_workspaces() {
@@ -14,6 +15,37 @@ function get_default_workspaces() {
     "tab_counts": Array(10).fill(0),
     "relative_focus_index": Array(10).fill(0),
     "active": 1
+  }
+}
+
+
+function hasSameSchema(a, b) {
+  if(typeof a !== typeof b) return false;
+  if(typeof a === 'object') {
+    if(a === null) return b === null;
+    if(a instanceof Date) return b instanceof Date && a.getTime() === b.getTime();
+    if(Array.isArray(a)) {
+      return Array.isArray(b) && a.length === b.length && a.every((val, index) => hasSameSchema(val, b[index]));
+    }
+    if(Object.keys(a).length !== Object.keys(b).length) return false;
+    return Object.entries(a).every(([key, val]) => hasSameSchema(val, b[key]));
+  }
+  return true;
+}
+
+
+async function initial_storage_check() {
+  const received_workspaces = await browser.storage.local.get(["workspaces"]);
+  if (hasSameSchema(workspaces, received_workspaces.workspaces)) {
+    workspaces = received_workspaces.workspaces;
+    console.log("schema same");
+    console.log("workspaces", workspaces);
+    console.log("received_workspaces", received_workspaces.workspaces);
+  } else {
+    await browser.storage.local.set({"workspaces": workspaces});
+    console.log("schema differs!!!");
+    console.log("workspaces", workspaces);
+    console.log("received_workspaces", received_workspaces.workspaces);
   }
 }
 
@@ -30,8 +62,9 @@ async function check_for_persistent_storage() {
 
 
 function is_number(string) {
-  return !isNaN(parseInt(string));
+  return !Number.isNaN(parseInt(string));
 }
+
 
 browser.commands.onCommand.addListener(async (command) => {
   await check_for_persistent_storage();
@@ -51,14 +84,14 @@ browser.commands.onCommand.addListener(async (command) => {
 
 
 async function workspace_schtick(next_workspace) {
-  const active_workspace = workspaces['active'];
-  if (next_workspace == active_workspace) return;
+  const active_workspace = workspaces.active;
+  if (next_workspace === active_workspace) return;
 
   const visible_tabs = await get_visible_tabs();
   const visible_tab_ids = visible_tabs.map(({ id }) => id);
   const visible_tab_count = visible_tabs.length;
 
-  const relative_focused_tab_index = visible_tabs.findIndex(tab => tab.active==true);
+  const relative_focused_tab_index = visible_tabs.findIndex(tab => tab.active===true);
 
   const current_workspace_start = get_start_index_of_workspace(active_workspace);
   await browser.tabs.move(visible_tab_ids, { index: current_workspace_start });
@@ -68,9 +101,9 @@ async function workspace_schtick(next_workspace) {
   workspaces.active = next_workspace;
 
   // round two
-  const next_workspace_length = workspaces['tab_counts'][next_workspace];
+  const next_workspace_length = workspaces.tab_counts[next_workspace];
 
-  if (next_workspace_length == 0) {
+  if (next_workspace_length === 0) {
     const new_tab = await browser.tabs.create({}); // can't set -1
     await browser.tabs.move(new_tab.id, { index: -1 });
     workspaces.tab_counts[next_workspace] = 1;
@@ -93,13 +126,13 @@ async function workspace_schtick(next_workspace) {
 
 async function get_visible_tabs() {
   const tabs = await browser.tabs.query({ currentWindow: true });
-  const visible_tabs = tabs.filter(tab => tab.hidden == false);
+  const visible_tabs = tabs.filter(tab => tab.hidden === false);
   return visible_tabs;
 }
 
 
 function get_start_index_of_workspace(target) {
-  return workspaces['tab_counts'].slice(0, target).reduce((a, b) => a + b, 0);
+  return workspaces.tab_counts.slice(0, target).reduce((a, b) => a + b, 0);
 }
 
 async function get_tabs_by_index(start, end) {
@@ -114,10 +147,10 @@ async function show_all_tabs() {
   const all_tabs = await browser.tabs.query({ currentWindow: true });
   const all_tab_ids = all_tabs.map(({ id }) => id);
 
-  const visible_tabs = all_tabs.filter(tab => tab.hidden == false);
+  const visible_tabs = all_tabs.filter(tab => tab.hidden === false);
   const visible_tab_ids = visible_tabs.map(({ id }) => id);
 
-  const active_workspace_start_index = get_start_index_of_workspace(workspaces['active']);
+  const active_workspace_start_index = get_start_index_of_workspace(workspaces.active);
 
   await browser.tabs.move(visible_tab_ids, { index: active_workspace_start_index });
   await browser.tabs.show(all_tab_ids);
